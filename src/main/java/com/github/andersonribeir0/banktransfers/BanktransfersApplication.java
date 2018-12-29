7ecc534c84ab6400074f96c0e33ed596a7afdd73
Anderson Ribeiro <anderson.moouro@gmail.com>
2018-12-29 18:58:49 -0200

    Initial commit

---
+++ src/main/java/com/github/andersonribeir0/banktransfers/BanktransfersApplication.java
@@ -0,0 +1,14 @@
+package com.github.andersonribeir0.banktransfers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanktransfersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanktransfersApplication.class, args);
+    }
+
+}
+
